ce944c444542b62495d9fbe2bff426f4d2b92131
ksyao <ksyao2002@gmail.com>
2024-09-29 19:00:39 -0500

    update

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,15 @@ pub mod tests;
 pub struct Protorune(());
 
 impl Protorune {
+    pub fn index_runestone<T: MessageContext>(block: Block) {
+        let mut runestones: Vec<Artifact> = Vec::new();
+        for tx in block.txdata.iter() {
+            if let Some(runestone) = Runestone::decipher(tx) {
+                runestones.push(runestone);
+            }
+        }
+    }
+
     pub fn index_block<T: MessageContext>(block: Block, height: u32) -> Result<()> {
         constants::HEIGHT_TO_BLOCKHASH
             .select_value::<u32>(height)
@@ -26,15 +35,14 @@ impl Protorune {
         constants::BLOCKHASH_TO_HEIGHT
             .select(&block.block_hash().as_byte_array().to_vec())
             .set_value::<u32>(height);
-        flush();
-        let _runestones: Vec<Option<Artifact>> = block
-            .txdata
-            .iter()
-            .map(|tx| Runestone::decipher(tx))
-            .collect();
+
+        if (height >= constants::GENESIS) {
+            Self::index_runestone::<T>(block);
+        }
 
         let _protocol_tag = T::protocol_tag();
         println!("got block");
+        flush();
         Ok(())
     }
 }
