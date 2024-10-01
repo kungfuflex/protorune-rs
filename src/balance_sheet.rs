7e2c6cbdbc947f6bc26167e8e1fc65e57770f787
kungfuflex <judoflexchop@gmail.com>
2024-10-01 15:17:52 +0000

    update cargo.lock

--- src/balance_sheet.rs
+++ src/balance_sheet.rs
@@ -1,4 +1,4 @@
-use metashrew_rs::index_pointer::IndexPointer;
+use metashrew_rs::index_pointer::{IndexPointer, KeyValuePointer};
 use ordinals::RuneId;
 use std::collections::HashMap;
 use std::ops::Deref;
