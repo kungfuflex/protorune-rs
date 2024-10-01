d2ca7deed16d700b67a1df7e0a79487073cb776a
kungfuflex <judoflexchop@gmail.com>
2024-10-01 15:55:04 +0000

    fix compile errors

--- src/protostone.rs
+++ src/protostone.rs
@@ -20,29 +20,28 @@ type Result<T> = std::result::Result<T, ProtostoneError>;
 impl fmt::Display for ProtostoneError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let s = match self {
-            Self::EncodeError => "Error encoding protostone",
-            Self::NoProtostoneError => "No protostone found",
-            Self::Flawed => "Corrupted protostonea",
+            Self::Encode => "error encoding protostone",
+            Self::NoProtostone => "no protostone found",
+            Self::Flawed => "corrupted protostonea",
+            Self::VarintError(_) => "varint error"
         };
-        write!(f, s)
+        write!(f, "{}", s)
     }
 }
 
-impl Bytes for u128 {}
-
 pub struct Protostone {
     burn: Option<u32>,
-    message: Option<u32>,
+    message: Vec<u128>,
     edicts: Option<Vec<Edict>>,
     refund: Option<u32>,
     pointer: Option<u32>,
     from: Option<u32>,
 }
 
-fn varint_byte_len(input: Vec<u8>, n: u128) -> Result<usize> {
+fn varint_byte_len(input: &Vec<u8>, n: u128) -> Result<usize> {
     let mut cloned = input.clone();
     for i in 0..n {
-        let (_, size) = varint::decode(&cloned.as_slice())?;
+        let (_, size) = varint::decode(&cloned.as_slice()).map_err(|e| ProtostoneError::VarintError(e))?;
         cloned.drain(0..size);
     }
 
@@ -50,63 +49,67 @@ fn varint_byte_len(input: Vec<u8>, n: u128) -> Result<usize> {
 }
 
 impl Protostone {
-    pub fn append_edicts(&mut self, edicts: Vec<Edict>) -> Self {
+    pub fn append_edicts(&mut self, edicts: Vec<Edict>) {
         self.edicts = Some(edicts);
-        self
     }
 
     pub fn from_bytes(tx: &Transaction, bytes: Vec<u8>) -> Result<Self> {
-        let integers = Runestone::integers(&bytes.as_slice())?;
+        let integers = Runestone::integers(&bytes.as_slice()).map_err(|e| ProtostoneError::VarintError(e))?;
         let Message {
             edicts,
             flaw,
             mut fields,
-        } = Message::from_integers(tx, bytes);
+        } = Message::from_integers(tx, &integers);
         // Can either throw or not throw
-        if Some(flaw) {
-            Err(ProtostoneError::Flawed)
+        if let Some(_) = flaw {
+            return Err(ProtostoneError::Flawed);
         }
 
         Ok(Protostone {
-            burn: Tag::Burn.take(&mut fields, |[tag]| Some(tag)),
-            message: Tag::Message.take(&mut fields, |[tag]| Some(tag)),
-            refund: Tag::Refund.take(&mut fields, |[tag]| Some(tag)),
-            pointer: Tag::ProtoPointer.take(&mut fields, |[tag]| Some(tag)),
-            from: Tag::From.take(&mut fields, |[tag]| Some(tag)),
+            burn: Tag::Burn.take(&mut fields, |[tag]| Some(tag as u32)),
+            message: match fields.get(&<u128 as From<Tag>>::from(Tag::Message)) {
+              Some(v) => v.clone().try_into().map_err(|_| ProtostoneError::Flawed)?,
+              None => Vec::<u128>::new()
+            },
+            refund: Tag::Refund.take(&mut fields, |[tag]| Some(tag as u32)),
+            pointer: Tag::ProtoPointer.take(&mut fields, |[tag]| Some(tag as u32)),
+            from: Tag::From.take(&mut fields, |[tag]| Some(tag as u32)),
             edicts: Some(edicts),
         })
     }
 
     pub fn from_runestone(tx: &Transaction, runestone: &Runestone) -> Result<Vec<Self>> {
-        let protostone_raw = runestone.proto.ok_or(ProtostoneError::NoProtostone)?;
+        let protostone_raw = runestone.proto.clone().ok_or(ProtostoneError::NoProtostone)?;
+        let protostone_raw_len = protostone_raw.len();
         let mut protostone_bytes = protostone_raw
             .into_iter()
             .enumerate()
-            .map(|(i, v)| {
-                if i == protostone_raw.len() - 1 {
-                    ByteUtils::snap_to_15_bytes(v)
+            .map(|(i, v)| -> Vec<u8> {
+                if i == protostone_raw_len - 1 {
+                    <u128 as ByteUtils>::snap_to_15_bytes(v)
+                } else {
+                  <u128 as ByteUtils>::to_aligned_bytes(v)
                 }
-                ByteUtils::to_aligned_bytes(v)
             })
             .flatten()
             .collect::<Vec<u8>>();
         let mut protostones: Vec<Self> = vec![];
         while protostone_bytes.len() > 0 {
-            let (protocol_tag, size) = varint::decode(&protostone_bytes.as_slice())?;
+            let (protocol_tag, size) = varint::decode(&protostone_bytes.as_slice()).map_err(|e| ProtostoneError::VarintError(e))?;
             if protocol_tag == 0 {
                 break;
             }
-            if size == u128::MAX {
+            if size == usize::MAX {
                 break;
             }
             protostone_bytes.drain(0..size);
-            let (len, size) = varint::decode(&protostone_bytes.as_slice())?;
-            if size == u128::MAX {
+            let (len, size) = varint::decode(&protostone_bytes.as_slice()).map_err(|e| ProtostoneError::VarintError(e))?;
+            if size == usize::MAX {
                 break;
             }
             protostone_bytes.drain(0..size);
-            let byte_length = varint_byte_len(input, len);
-            protostones.push(Protostone::from_bytes(protostone_bytes[0..byte_length]));
+            let byte_length = varint_byte_len(&protostone_bytes, len)?;
+            protostones.push(Protostone::from_bytes(tx, (&protostone_bytes[0..byte_length]).to_vec())?);
             protostone_bytes.drain(0..byte_length);
         }
 
