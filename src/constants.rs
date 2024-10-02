e851f56dfcddfb7e0836d36bff6c50c8448b1d18
phuncti0n <zmill777@proton.me>
2024-10-02 14:39:48 -0400

    start

--- src/constants.rs
+++ src/constants.rs
@@ -1,7 +1,7 @@
 use bitcoin::blockdata::block::Block;
 use bitcoin::hashes::Hash;
-use bitcoin::Network::{Bitcoin, Regtest};
-use metashrew::index_pointer::{IndexPointer, KeyValuePointer};
+use bitcoin::Network::{ Bitcoin, Regtest };
+use metashrew::index_pointer::{ IndexPointer, KeyValuePointer };
 use once_cell::sync::Lazy;
 use std::sync::atomic::AtomicPtr;
 use std::sync::Arc;
@@ -10,62 +10,87 @@ pub const RUNESTONE_TAG: u16 = 0x5d6a;
 pub const OP_RETURN: u8 = 0x6a;
 pub const GENESIS: u32 = 840000;
 pub const NETWORK: bitcoin::Network = Bitcoin;
+pub const MINIMUM_NAME: u128 = 99246114928149462;
+pub const RESERVED_NAME: u128 = 6402364363415443603228541259936211926;
+pub const TWENTY_SIX: u128 = 26;
 
-pub static HEIGHT_TO_BLOCKHASH: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/blockhash/byheight/"));
-pub static BLOCKHASH_TO_HEIGHT: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/height/byblockhash/"));
-pub static OUTPOINT_TO_RUNES: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/runes/byoutpoint/"));
-pub static HEIGHT_TO_RUNES: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/runes/byheight/"));
+pub static HEIGHT_TO_BLOCKHASH: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/blockhash/byheight/")
+);
+pub static BLOCKHASH_TO_HEIGHT: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/height/byblockhash/")
+);
+pub static OUTPOINT_TO_RUNES: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/runes/byoutpoint/")
+);
+pub static HEIGHT_TO_RUNES: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/runes/byheight/")
+);
 
-pub static ADDRESS_TO_RUNES: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/runes/byaddress/"));
+pub static ADDRESS_TO_RUNES: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/runes/byaddress/")
+);
 
-pub static ADDRESS_TO_PROTORUNES: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/protorunes/byaddress/"));
+pub static ADDRESS_TO_PROTORUNES: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/protorunes/byaddress/")
+);
 
-pub static HEIGHT_TO_PROTORUNES: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/protorunes/byheight/"));
+pub static HEIGHT_TO_PROTORUNES: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/protorunes/byheight/")
+);
 
-pub static OUTPOINTS_FOR_ADDRESS: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/outpoint/byaddress/"));
+pub static OUTPOINTS_FOR_ADDRESS: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/outpoint/byaddress/")
+);
 
-pub static OUTPOINT_SPENDABLE_BY: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/outpoint/spendableby/"));
-pub static OUTPOINT_TO_OUTPUT: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/output/byoutpoint/"));
-pub static OUTPOINT_TO_HEIGHT: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/height/byoutpoint/"));
-pub static HEIGHT_TO_TRANSACTION_IDS: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/txids/byheight"));
+pub static OUTPOINT_SPENDABLE_BY: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/outpoint/spendableby/")
+);
+pub static OUTPOINT_TO_OUTPUT: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/output/byoutpoint/")
+);
+pub static OUTPOINT_TO_HEIGHT: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/height/byoutpoint/")
+);
+pub static HEIGHT_TO_TRANSACTION_IDS: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/txids/byheight")
+);
 
 pub static SYMBOL: Lazy<IndexPointer> = Lazy::new(|| IndexPointer::from_keyword("/runes/symbol/"));
 pub static CAP: Lazy<IndexPointer> = Lazy::new(|| IndexPointer::from_keyword("/runes/cap/"));
 pub static SPACERS: Lazy<IndexPointer> = Lazy::new(|| IndexPointer::from_keyword("/runes/spaces/"));
-pub static OFFSETEND: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/runes/offset/end/"));
-pub static OFFSETSTART: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/runes/offset/start/"));
-pub static HEIGHTSTART: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/runes/height/start/"));
-pub static HEIGHTEND: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/runes/height/end/"));
+pub static OFFSETEND: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/runes/offset/end/")
+);
+pub static OFFSETSTART: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/runes/offset/start/")
+);
+pub static HEIGHTSTART: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/runes/height/start/")
+);
+pub static HEIGHTEND: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/runes/height/end/")
+);
 pub static AMOUNT: Lazy<IndexPointer> = Lazy::new(|| IndexPointer::from_keyword("/runes/amount/"));
-pub static MINTS_REMAINING: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/runes/mints-remaining/"));
-pub static PREMINE: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/runes/premine/"));
-pub static DIVISIBILITY: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/runes/divisibility/"));
-pub static RUNE_ID_TO_HEIGHT: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/height/byruneid/"));
+pub static MINTS_REMAINING: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/runes/mints-remaining/")
+);
+pub static PREMINE: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/runes/premine/")
+);
+pub static DIVISIBILITY: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/runes/divisibility/")
+);
+pub static RUNE_ID_TO_HEIGHT: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/height/byruneid/")
+);
 pub static ETCHINGS: Lazy<IndexPointer> = Lazy::new(|| IndexPointer::from_keyword("/runes/names"));
-pub static RUNE_ID_TO_ETCHING: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/etching/byruneid/"));
-pub static ETCHING_TO_RUNE_ID: Lazy<IndexPointer> =
-    Lazy::new(|| IndexPointer::from_keyword("/runeid/byetching/"));
+pub static RUNE_ID_TO_ETCHING: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/etching/byruneid/")
+);
+pub static ETCHING_TO_RUNE_ID: Lazy<IndexPointer> = Lazy::new(||
+    IndexPointer::from_keyword("/runeid/byetching/")
+);
 
 pub const SUBSIDY_HALVING_INTERVAL: u64 = 210_000;
 pub const HEIGHT_INTERVAL: u64 = 17_500;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use crate::message::MessageContext;
-use anyhow::{ anyhow, Result };
+use anyhow::{ anyhow, Ok, Result };
 use bitcoin::blockdata::block::Block;
 use bitcoin::consensus::encode::serialize;
 use bitcoin::hashes::Hash;
@@ -10,6 +10,7 @@ use ordinals::Etching;
 use ordinals::{ Artifact, Runestone };
 use protostone::{ add_to_indexable_protocols, initialized_protocol_index };
 use std::fmt::Write;
+use std::ops::Sub;
 use std::sync::Arc;
 
 pub mod balance_sheet;
@@ -26,23 +27,132 @@ pub struct Protorune(());
 
 impl Protorune {
     pub fn index_runestone<T: MessageContext>(
-        tx: &Transaction,
         runestone: &Runestone,
-        height: u32
+        height: u32,
+        index: u32
     ) -> Result<()> {
         if let Some(etching) = runestone.etching.as_ref() {
-            Self::index_etching(etching)?;
+            Self::index_etching(etching, index, height)?;
         }
         Ok(())
     }
-    pub fn index_etching(etching: &Etching) -> Result<()> {
+    pub fn index_etching(etching: &Etching, index: u32, height: u32) -> Result<()> {
+        if etching.rune.is_none() {
+            return Ok(());
+        }
+        let name: u128;
+        name = etching.rune.unwrap().0;
+        Self::get_reserved_name(height, index, name);
+
         Ok(())
     }
 
+    fn get_reserved_name(height: u32, index: u32, name: u128) {
+        let mut interval: i64 =
+            ((height - constants::GENESIS) as i64) / (constants::HEIGHT_INTERVAL as i64);
+        let mut minimum_name: u128 = constants::MINIMUM_NAME;
+        while interval > 0 {
+            minimum_name = minimum_name.sub(1) / constants::TWENTY_SIX;
+            interval -= 1;
+        }
+        if name {
+            if name < minimum_name || name >= constants::RESERVED_NAME {
+            }
+        }
+    }
+
+    //     const name = nameToArrayBuffer("UNCOMMONGOODS");
+    //     const spacers = 128;
+    //     const runeId = new RuneId(1, 0).toBytes();
+    //     ETCHING_TO_RUNE_ID.select(name).set(runeId);
+    //     RUNE_ID_TO_ETCHING.select(runeId).set(name);
+    //     RUNE_ID_TO_HEIGHT.select(runeId).setValue<u32>(GENESIS);
+    //     DIVISIBILITY.select(name).setValue<u8>(1);
+    //     AMOUNT.select(name).set(toArrayBuffer(u128.from(1)));
+    //     CAP.select(name).set(toArrayBuffer(u128.Max));
+    //     MINTS_REMAINING.select(name).set(toArrayBuffer(u128.Max));
+    //     OFFSETEND.select(name).setValue<u64>(SUBSIDY_HALVING_INTERVAL);
+    //     SPACERS.select(name).setValue<u32>(128);
+    //     SYMBOL.select(name).setValue<u8>(<u8>"\u{29C9}".charCodeAt(0));
+    //     ETCHINGS.append(name);
+    //   }
+
+    //   buildRuneId(height: u64, tx: u32): ArrayBuffer {
+    //     const runeId = new RuneId(height, tx).toBytes();
+    //     HEIGHT_TO_RUNE_IDS.selectValue<u32>(<u32>height).append(runeId);
+    //     return runeId;
+    //   }
+
+    //       etch(
+    //     height: u64,
+    //     tx: u32,
+    //     initialBalanceSheet: BalanceSheet,
+    //     transaction: RunesTransaction,
+    //   ): bool {
+    //     if (!this.isEtching()) return false;
+    //     const name = this.getReservedNameFor(height, tx);
+    //     if (isNullPtr<ArrayBuffer>(name)) return false;
+    //     if (ETCHING_TO_RUNE_ID.select(name).get().byteLength !== 0) return false; // already taken / commitment not foun
+    //     const runeId = this.buildRuneId(height, tx);
+    //     RUNE_ID_TO_ETCHING.select(runeId).set(name);
+    //     ETCHING_TO_RUNE_ID.select(name).set(runeId);
+    //     RUNE_ID_TO_HEIGHT.select(runeId).setValue<u32>(<u32>height);
+    //     if (this.fields.has(Field.DIVISIBILITY))
+    //       DIVISIBILITY.select(name).setValue<u8>(
+    //         fieldTo<u8>(this.fields.get(Field.DIVISIBILITY)),
+    //       );
+    //     if (this.fields.has(Field.PREMINE)) {
+    //       const premine = fieldToU128(this.fields.get(Field.PREMINE));
+    //       BalanceSheet.fromPairs([runeId], [premine]).pipe(initialBalanceSheet);
+    //       PREMINE.select(name).set(toArrayBuffer(premine));
+    //     }
+    //     if (this.getFlag(Flag.TERMS)) {
+    //       if (this.fields.has(Field.AMOUNT))
+    //         AMOUNT.select(name).set(
+    //           toArrayBuffer(fieldToU128(this.fields.get(Field.AMOUNT))),
+    //         );
+
+    //       if (this.fields.has(Field.CAP)) {
+    //         CAP.select(name).set(
+    //           toArrayBuffer(fieldToU128(this.fields.get(Field.CAP))),
+    //         );
+    //         MINTS_REMAINING.select(name).set(
+    //           fieldToArrayBuffer(this.fields.get(Field.CAP)),
+    //         );
+    //       }
+    //       if (this.fields.has(Field.HEIGHTSTART))
+    //         HEIGHTSTART.select(name).setValue<u64>(
+    //           fieldTo<u64>(this.fields.get(Field.HEIGHTSTART)),
+    //         );
+    //       if (this.fields.has(Field.HEIGHTEND))
+    //         HEIGHTEND.select(name).setValue<u64>(
+    //           fieldTo<u64>(this.fields.get(Field.HEIGHTEND)),
+    //         );
+    //       if (this.fields.has(Field.OFFSETSTART))
+    //         OFFSETSTART.select(name).setValue<u64>(
+    //           fieldTo<u64>(this.fields.get(Field.OFFSETSTART)),
+    //         );
+    //       if (this.fields.has(Field.OFFSETEND))
+    //         OFFSETEND.select(name).setValue<u64>(
+    //           fieldTo<u64>(this.fields.get(Field.OFFSETEND)),
+    //         );
+    //     }
+    //     if (this.fields.has(Field.SPACERS))
+    //       SPACERS.select(name).setValue<u32>(
+    //         fieldTo<u32>(this.fields.get(Field.SPACERS)),
+    //       );
+    //     if (this.fields.has(Field.SYMBOL))
+    //       SYMBOL.select(name).setValue<u8>(
+    //         fieldTo<u8>(this.fields.get(Field.SYMBOL)),
+    //       );
+    //     ETCHINGS.append(name);
+    //     return true;
+    //   }
+
     pub fn index_unspendables<T: MessageContext>(block: &Block, height: u32) -> Result<()> {
-        for tx in block.txdata.iter() {
+        for (index, tx) in block.txdata.iter().enumerate() {
             if let Some(Artifact::Runestone(ref runestone)) = Runestone::decipher(tx) {
-                Self::index_runestone::<T>(tx, runestone, height)?;
+                Self::index_runestone::<T>(tx, runestone, height, index)?;
             }
         }
         Ok(())
