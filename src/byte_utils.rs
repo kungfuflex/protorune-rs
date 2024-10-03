d3a3de41d683ef3ee8da59e708f538d1c3abc486
kungfuflex <judoflexchop@gmail.com>
2024-10-03 22:13:13 +0000

    fix byteutils

--- src/byte_utils.rs
+++ src/byte_utils.rs
@@ -1,14 +1,14 @@
 use metashrew::byte_view::{shrink_back, ByteView};
 
 pub trait ByteUtils {
-    fn to_aligned_bytes(v: Self) -> Vec<u8>;
-    fn snap_to_15_bytes(v: Self) -> Vec<u8>;
-    fn to_u32(v: Self) -> u32;
+    fn to_aligned_bytes(self) -> Vec<u8>;
+    fn snap_to_15_bytes(self) -> Vec<u8>;
+    fn to_u32(self) -> u32;
 }
 
 impl ByteUtils for u128 {
-    fn to_aligned_bytes(v: Self) -> Vec<u8> {
-        let mut ar = v.to_bytes();
+    fn to_aligned_bytes(self) -> Vec<u8> {
+        let mut ar: Vec<u8> = (self.to_le_bytes()).try_into().unwrap();
         let mut end = 0;
         for (_i, v) in ar.iter().enumerate() {
             if *v != 0 {
@@ -20,14 +20,13 @@ impl ByteUtils for u128 {
         ar.drain(0..end);
         ar
     }
-    fn snap_to_15_bytes(v: Self) -> Vec<u8> {
-        let mut ar = v.to_bytes();
+    fn snap_to_15_bytes(self) -> Vec<u8> {
+        let mut ar: Vec<u8> = (self.to_le_bytes()).try_into().unwrap();
         ar.drain(0..1);
         ar
     }
 
-    fn to_u32(v: Self) -> u32 {
-        let ar = v.to_bytes();
-        u32::from_bytes(ar[0..4].to_vec())
+    fn to_u32(self) -> u32 {
+      self as u32
     }
 }
