3f421dc615c0a10bd5e73990b984a39601315f75
phuncti0n <zmill777@proton.me>
2024-10-04 17:51:02 -0400

    test to check balance sheet stores correctly

--- src/tests/index_runes.rs
+++ src/tests/index_runes.rs
@@ -59,100 +59,100 @@ mod tests {
         hex_string
     }
 
-    // #[wasm_bindgen_test]
-    // fn protorune_creation() {
-    //     clear();
-    //     let test_block = helpers::create_block_with_coinbase(840000);
-    //     let expected_block_hash = display_vec_as_hex(
-    //         test_block.block_hash().as_byte_array().to_vec()
-    //     );
-    //     let _ = Protorune::index_block::<MyMessageContext>(test_block, 840000);
-    //     tables::OUTPOINTS_FOR_ADDRESS
-    //         .keyword("bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu")
-    //         .set(Arc::new(Vec::new()));
-    //     let test_val = IndexPointer::from_keyword("/blockhash/byheight/")
-    //         .select_value(840000 as u32)
-    //         .get();
-    //     let hex_str = display_vec_as_hex((*test_val).clone());
-    //     println!("{}", hex_str);
-    //     //assert_eq!(hex_str, expected_block_hash);
-    // }
+    #[wasm_bindgen_test]
+    fn protorune_creation() {
+        clear();
+        let test_block = helpers::create_block_with_coinbase(840000);
+        let expected_block_hash = display_vec_as_hex(
+            test_block.block_hash().as_byte_array().to_vec()
+        );
+        let _ = Protorune::index_block::<MyMessageContext>(test_block, 840000);
+        tables::OUTPOINTS_FOR_ADDRESS
+            .keyword("bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu")
+            .set(Arc::new(Vec::new()));
+        let test_val = IndexPointer::from_keyword("/blockhash/byheight/")
+            .select_value(840000 as u32)
+            .get();
+        let hex_str = display_vec_as_hex((*test_val).clone());
+        println!("{}", hex_str);
+        //assert_eq!(hex_str, expected_block_hash);
+    }
 
-    // #[wasm_bindgen_test]
-    // fn spendable_by_address() {
-    //     clear();
-    //     let test_block = helpers::create_block_with_tx(false);
-    //     let _ = Protorune::index_block::<MyMessageContext>(test_block.clone(), 840001);
-    //     tables::OUTPOINTS_FOR_ADDRESS
-    //         .keyword("bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu")
-    //         .set(Arc::new(Vec::new()));
-    //     let outpoint: OutPoint = OutPoint {
-    //         txid: Txid::from_str(
-    //             "a440cb400062f14cff5f76fbbd3881c426820171180c67c103a36d12c89fbd32"
-    //         ).unwrap(),
-    //         vout: 0,
-    //     };
-    //     let test_val = tables::OUTPOINT_SPENDABLE_BY.select(&serialize(&outpoint)).get();
-    //     let addr_str = display_vec_as_hex(test_val.to_vec());
-    //     let _addr_str: String = display_vec_as_hex(
-    //         "bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu".to_string().into_bytes()
-    //     );
+    #[wasm_bindgen_test]
+    fn spendable_by_address() {
+        clear();
+        let test_block = helpers::create_block_with_tx(false);
+        let _ = Protorune::index_block::<MyMessageContext>(test_block.clone(), 840001);
+        tables::OUTPOINTS_FOR_ADDRESS
+            .keyword("bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu")
+            .set(Arc::new(Vec::new()));
+        let outpoint: OutPoint = OutPoint {
+            txid: Txid::from_str(
+                "a440cb400062f14cff5f76fbbd3881c426820171180c67c103a36d12c89fbd32"
+            ).unwrap(),
+            vout: 0,
+        };
+        let test_val = tables::OUTPOINT_SPENDABLE_BY.select(&serialize(&outpoint)).get();
+        let addr_str = display_vec_as_hex(test_val.to_vec());
+        let _addr_str: String = display_vec_as_hex(
+            "bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu".to_string().into_bytes()
+        );
 
-    //     let view_test = View::outpoints_by_address(
-    //         "bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu".to_string().into_bytes()
-    //     );
-    //     let mut outpoint_vec: Vec<String> = Vec::new();
-    //     outpoint_vec.push(
-    //         "a440cb400062f14cff5f76fbbd3881c426820171180c67c103a36d12c89fbd32:0".to_string()
-    //     );
-    //     let matching_view_test = view::AddressOutpoints {
-    //         outpoints: outpoint_vec,
-    //     };
-    //     assert_eq!(view_test, serde_json::to_string_pretty(&matching_view_test).unwrap());
-    //     assert_eq!(_addr_str, addr_str);
-    // }
+        let view_test = View::outpoints_by_address(
+            "bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu".to_string().into_bytes()
+        );
+        let mut outpoint_vec: Vec<String> = Vec::new();
+        outpoint_vec.push(
+            "a440cb400062f14cff5f76fbbd3881c426820171180c67c103a36d12c89fbd32:0".to_string()
+        );
+        let matching_view_test = view::AddressOutpoints {
+            outpoints: outpoint_vec,
+        };
+        assert_eq!(view_test, serde_json::to_string_pretty(&matching_view_test).unwrap());
+        assert_eq!(_addr_str, addr_str);
+    }
 
-    // #[wasm_bindgen_test]
-    // fn outpoints_by_address() {
-    //     clear();
-    //     let test_block = helpers::create_block_with_tx(false);
-    //     let _ = Protorune::index_block::<MyMessageContext>(test_block.clone(), 840001);
-    //     let outpoint: OutPoint = OutPoint {
-    //         txid: Txid::from_str(
-    //             "a440cb400062f14cff5f76fbbd3881c426820171180c67c103a36d12c89fbd32"
-    //         ).unwrap(),
-    //         vout: 0,
-    //     };
-    //     let test_val = tables::OUTPOINTS_FOR_ADDRESS
-    //         .keyword("bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu")
-    //         .get_list();
-    //     let list_str: String = display_list_as_hex(test_val);
+    #[wasm_bindgen_test]
+    fn outpoints_by_address() {
+        clear();
+        let test_block = helpers::create_block_with_tx(false);
+        let _ = Protorune::index_block::<MyMessageContext>(test_block.clone(), 840001);
+        let outpoint: OutPoint = OutPoint {
+            txid: Txid::from_str(
+                "a440cb400062f14cff5f76fbbd3881c426820171180c67c103a36d12c89fbd32"
+            ).unwrap(),
+            vout: 0,
+        };
+        let test_val = tables::OUTPOINTS_FOR_ADDRESS
+            .keyword("bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu")
+            .get_list();
+        let list_str: String = display_list_as_hex(test_val);
 
-    //     let test_outpoint: Vec<u8> = serialize(&outpoint);
-    //     let outpoint_hex: String = display_vec_as_hex(test_outpoint);
+        let test_outpoint: Vec<u8> = serialize(&outpoint);
+        let outpoint_hex: String = display_vec_as_hex(test_outpoint);
 
-    //     assert_eq!(list_str, outpoint_hex);
-    // }
+        assert_eq!(list_str, outpoint_hex);
+    }
 
-    // #[wasm_bindgen_test]
-    // fn index_runestone() {
-    //     clear();
-    //     let test_block = helpers::create_block_with_tx(true);
-    //     tables::OUTPOINTS_FOR_ADDRESS
-    //         .keyword("bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu")
-    //         .set(Arc::new(Vec::new()));
-    //     let _ = Protorune::index_block::<MyMessageContext>(test_block.clone(), 840001);
-    //     let rune_id = Protorune::build_rune_id(840001, 0);
-    //     let test_val = tables::RUNES.RUNE_ID_TO_ETCHING.select(&rune_id).get();
-    //     let cache_hex: String = display_vec_as_hex(test_val.to_vec());
-    //     let rune = Rune::from_str("TESTER").unwrap().0.to_string().into_bytes();
-    //     let rune_hex: String = display_vec_as_hex(rune);
-    //     assert_eq!(rune_hex, cache_hex);
-    // }
+    #[wasm_bindgen_test]
+    fn index_runestone() {
+        clear();
+        let test_block = helpers::create_block_with_tx(true);
+        tables::OUTPOINTS_FOR_ADDRESS
+            .keyword("bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu")
+            .set(Arc::new(Vec::new()));
+        let _ = Protorune::index_block::<MyMessageContext>(test_block.clone(), 840001);
+        let rune_id = Protorune::build_rune_id(840001, 0);
+        let test_val = tables::RUNES.RUNE_ID_TO_ETCHING.select(&rune_id).get();
+        let cache_hex: String = display_vec_as_hex(test_val.to_vec());
+        let rune = Rune::from_str("TESTER").unwrap().0.to_string().into_bytes();
+        let rune_hex: String = display_vec_as_hex(rune);
+        assert_eq!(rune_hex, cache_hex);
+    }
 
     #[wasm_bindgen_test]
     fn correct_balance_sheet() {
-        //clear();
+        clear();
         let test_block = helpers::create_block_with_tx(true);
         let _ = Protorune::index_block::<MyMessageContext>(test_block.clone(), 840000);
         let outpoint: OutPoint = OutPoint {
@@ -166,11 +166,10 @@ mod tests {
             tx: 0 as u128,
         };
         let sheet = BalanceSheet::load(
-            &tables::RUNES.OUTPOINT_TO_RUNES.select(&outpoint.to_string().into_bytes())
+            &tables::RUNES.OUTPOINT_TO_RUNES.select(&consensus_encode(&outpoint).unwrap())
         );
-        println!("{:?}", sheet);
 
         let stored_balance = sheet.get(&protorune_id);
-        //assert_eq!(1000 as u128, stored_balance);
+        assert_eq!(1000 as u128, stored_balance);
     }
 }
